17503e3e37743e9f03154e39d9f026ca3676e26e
mtsmfm <mtsmfm@gmail.com>
2021-08-13 07:18:16 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Devfiles
+
+A collection of Devfiles and docker images for eclipse che.
+
+## Images
+
+- ghcr.io/mtsmfm/devfiles:node-16--main
+- ghcr.io/mtsmfm/devfiles:node-14--main
+- ghcr.io/mtsmfm/devfiles:node-12--main
+- ghcr.io/mtsmfm/devfiles:ruby-3-0--main
+- ghcr.io/mtsmfm/devfiles:ruby-2-7--main
+- ghcr.io/mtsmfm/devfiles:ruby-2-6--main
+
+https://github.com/mtsmfm/devfiles/pkgs/container/devfiles
---
+++ images/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eux
+
+local user_name=${USER_NAME:-user}
+
+if [ ! -d "${HOME}" ]; then
+  mkdir -p "${HOME}"
+fi
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${user_name}:x:$(id -u):0:${user_name} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${user_name}:x:$(id -u):" >> /etc/group
+    echo "${user_name}:!:$($(date +%s)/86400):0:99999:7:::" >> /etc/shadow
+  fi
+
+  if [ -d /etc/sudoers.d ]; then
+    echo user ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${user_name}
+    chmod 0440 /etc/sudoers.d/${user_name}
+  fi
+fi
+
+if curl --fail -I https://github.com/$CHE_WORKSPACE_NAMESPACE/dotfiles; then
+  git clone https://github.com/$CHE_WORKSPACE_NAMESPACE/dotfiles ~/dotfiles
+  ~/dotfiles/install
+else
+  git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
+  cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
+fi
+
+exec "$@"
